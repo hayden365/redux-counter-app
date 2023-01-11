478324f55b40e0c392bfe496d6ab5abe6eecab4f
Hyeji Kong <109419531+hayden365@users.noreply.github.com>
2023-01-11 21:44:14 +0900

    리덕스 로깅 미들웨어 만들기

---
+++ src/middleware/log.tsx
@@ -0,0 +1,8 @@
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+	console.log('store', store);
+	console.log('action', action);
+	next(action);
+};
+
+export default loggerMiddleware;
+
